c213622c887761a4393ece8a3d8ec03c2851f43a
xiewang <xiewang0501@126.com>
2017-09-25 23:14:55 +0800

    schedule add post

---
+++ src/server/add.js
@@ -0,0 +1,50 @@
+var mysql = require('mysql');
+var _ = require('lodash');
+var Promise = require('bluebird');
+var rp = require('request-promise');
+var log4js = require('log4js');
+var config = require('../constants.js');
+log4js.configure({
+    appenders: {
+        out: {type: 'stdout'},
+        app: {type: 'file', filename: 'application.log'}
+    },
+    categories: {
+        default: {appenders: ['out', 'app'], level: 'debug'}
+    }
+});
+var logger = log4js.getLogger();
+logger.level = 'debug';
+
+var connection = '';
+//start();
+
+function start() {
+    connection = mysql.createConnection({
+        host: config.host,
+        user: config.user,
+        password: config.password,
+        database: config.database,
+        insecureAuth: true
+    });
+    // connection.connect();
+    connection.connect(function (err) {              // The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(start, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+    connection.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            start();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+
+
+}
+
+module.exports = start;
+//connection.end();
