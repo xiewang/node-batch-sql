b91b24014da5a6f06de46b98b38aee0cb9bd661b
xiewang <xiewang0501@126.com>
2017-10-07 16:22:27 +0800

    complete cate

---
+++ src/server/cate.js
@@ -0,0 +1,332 @@
+var cate = function (id) {
+    /*
+    *   5，母婴
+    *   2，男装
+    *   3，女装
+    *   6，全部
+    *   7，居家
+    *   8，化妆品
+    *   9，数码家电
+    *   10，箱包配饰
+    *   11，美食
+    *   12，汽车用品
+    *   13，户外运动
+    * */
+    var cate = 16;
+
+    switch (id) {
+        case 26:
+            cate = 12;
+            break;
+        case 50020808:
+            cate = 7;
+            break;
+        case 50020857:
+            cate = 7;
+            break;
+        case 50025707:
+            cate = 13;
+            break;
+        case 30:
+            cate = 2;
+            break;
+        case 50008164:
+            cate = 7;
+            break;
+        case 50020611:
+            cate = 7;
+            break;
+        case 50023904:
+            cate = 9;
+            break;
+        case 50010788:
+            cate = 8;
+            break;
+        case 1801:
+            cate = 8;
+            break;
+        case 50023282:
+            cate = 8;
+            break;
+        case 1512:
+            cate = 9;
+            break;
+        case 14:
+            cate = 9;
+            break;
+        case 1201:
+            cate = 9;
+            break;
+        case 1101:
+            cate = 9;
+            break;
+        case 50019780:
+            cate = 9;
+            break;
+        case 50018222:
+            cate = 9;
+            break;
+        case 11:
+            cate = 9;
+            break;
+        case 50018264:
+            cate = 9;
+            break;
+        case 50008090:
+            cate = 9;
+            break;
+        case 50012164:
+            cate = 9;
+            break;
+        case 50007218:
+            cate = 9;
+            break;
+        case 50018004:
+            cate = 9;
+            break;
+        case 20:
+            cate = 9;
+            break;
+        case 50022703:
+            cate = 9;
+            break;
+        case 50011972:
+            cate = 9;
+            break;
+        case 50012100:
+            cate = 9;
+            break;
+        case 50012082:
+            cate = 9;
+            break;
+        case 50002768:
+            cate = 7;
+            break;
+        case 27:
+            cate = 7;
+            break;
+        case 50020332:
+            cate = 7;
+            break;
+        case 50020485:
+            cate = 7;
+            break;
+        case 50020579:
+            cate = 7;
+            break;
+        case 5001194:
+            cate = 13;
+            break;
+        case 21:
+            cate = 7;
+            break;
+        case 50016349:
+            cate = 7;
+            break;
+        case 50016348:
+            cate = 7;
+            break;
+        case 50008163:
+            cate = 7;
+            break;
+        case 35:
+            cate = 5;
+            break;
+        case 50014812:
+            cate = 5;
+            break;
+        case 50022517:
+            cate = 5;
+            break;
+        case 50008165:
+            cate = 5;
+            break;
+        case 50020276:
+            cate = 6;
+            break;
+        case 50020275:
+            cate = 6;
+            break;
+        case 50002766:
+            cate = 11;
+            break;
+        case 50016422:
+            cate = 11;
+            break;
+        case 50008075:
+            cate = 11;
+            break;
+        case 40:
+            cate = 6;
+            break;
+        case 50010728:
+            cate = 13;
+            break;
+        case 50013886:
+            cate = 13;
+            break;
+        case 50011699:
+            cate = 13;
+            break;
+        case 25:
+            cate = 5;
+            break;
+        case 50011665:
+            cate = 6;
+            break;
+        case 50008907:
+            cate = 6;
+            break;
+        case 99:
+            cate = 6;
+            break;
+        case 23:
+            cate = 6;
+            break;
+        case 50007216:
+            cate = 7;
+            break;
+        case 50004958:
+            cate = 6;
+            break;
+        case 50005700:
+            cate = 10;
+            break;
+        case 50011740:
+            cate = 2;
+            break;
+        case 16:
+            cate = 3;
+            break;
+        case 50006843:
+            cate = 3;
+            break;
+        case 50006842:
+            cate = 10;
+            break;
+        case 1625:
+            cate = 7;
+            break;
+        case 50010404:
+            cate = 7;
+            break;
+        case 50011397:
+            cate = 7;
+            break;
+        case 28:
+            cate = 7;
+            break;
+        case 33:
+            cate = 6;
+            break;
+        case 34:
+            cate = 6;
+            break;
+        case 50017300:
+            cate = 6;
+            break;
+        case 29:
+            cate = 7;
+            break;
+        case 29:
+            cate = 7;
+            break;
+        case 50012029:
+            cate = 2;
+            break;
+        case 50013864:
+            cate = 10;
+            break;
+        case 50018252:
+            cate = 6;
+            break;
+        case 50014442:
+            cate = 6;
+            break;
+        case 50014811:
+            cate = 6;
+            break;
+        case 50016891:
+            cate = 6;
+            break;
+        case 50023724:
+            cate = 6;
+            break;
+        case 50017908:
+            cate = 6;
+            break;
+        case 50017652:
+            cate = 6;
+            break;
+        case 50019379:
+            cate = 6;
+            break;
+        case 50023575:
+            cate = 6;
+            break;
+        case 50023717:
+            cate = 7;
+            break;
+        case 50023878:
+            cate = 7;
+            break;
+        case 50024186:
+            cate = 6;
+            break;
+        case 50024449:
+            cate = 6;
+            break;
+        case 50024451:
+            cate = 6;
+            break;
+        case 50024612:
+            cate = 6;
+            break;
+        case 50024971:
+            cate = 7;
+            break;
+        case 50025004:
+            cate = 6;
+            break;
+        case 50025110:
+            cate = 6;
+            break;
+        case 50025111:
+            cate = 6;
+            break;
+        case 50025490:
+            cate = 6;
+            break;
+        case 50025616:
+            cate = 7;
+            break;
+        case 50025618:
+            cate = 6;
+            break;
+        case 50025705:
+            cate = 7;
+            break;
+        case 50025706:
+            cate = 6;
+            break;
+        case 50025968:
+            cate = 6;
+            break;
+        case 50026316:
+            cate = 11;
+            break;
+        case 50023804:
+            cate = 7;
+            break;
+        case 50026523:
+            cate = 6;
+            break;
+        default:
+            cate = 6;
+            break;
+    }
+    return cate;
+};
+
+
+module.exports = start;
